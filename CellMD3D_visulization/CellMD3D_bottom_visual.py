b8748388d5959625e473da3a95f9ff80aa4588b2
Chu Pan <34529833+MinTTT@users.noreply.github.com>
2023-06-29 02:39:13 +0800

    plot cells in 2d

---
+++ CellMD3D_visulization/CellMD3D_bottom_visual.py
@@ -0,0 +1,105 @@
+"""
+@author: CHU Pan
+@mail: pan_chu@outlook.com
+@date: 2023/6/29
+"""
+
+# %%
+import matplotlib.path as mpath
+import matplotlib.patches as mpatches
+import matplotlib.pyplot as plt
+import os
+import sciplot as splt
+import numpy as np
+
+splt.whitegrid()
+from CellMD3D_visulization.CellMD3D_utility import read_cells_rets
+
+
+def cell_vertices(radius, p1, p2, num=40):
+    if num % 2 != 0:
+        num += 1
+    vertices_num = num
+    direction = (np.array(p2) - np.array(p1))
+    norm_direction = direction / np.sqrt(np.sum(direction ** 2))
+
+    vertical = np.array([1, - norm_direction[0] / norm_direction[1]])
+    norm_vertical = vertical / np.sqrt(np.sum(vertical ** 2))
+    norm_vertical_2 = -norm_vertical
+    mid_point = (np.array(p2) + np.array(p1)) / 2
+    vertical_point_1 = norm_vertical * radius + mid_point
+    vertical_point_2 = norm_vertical_2 * radius + mid_point
+    start_phi = np.arctan(norm_vertical[1] / norm_vertical[0])
+    rho = np.linspace(start_phi, start_phi + np.pi * 2, vertices_num, endpoint=True)
+    half_index = int(vertices_num / 2)
+    vertices = np.ones((vertices_num, 2))
+    vertices[:, 0] = np.cos(rho) * radius
+    # vertices[half_index+1:-1, 0] = np.cos(rho[half_index-1:]) * radius
+
+    vertices[:, 1] = np.sin(rho) * radius
+    # vertices[half_index+1:-1, 1] = np.sin(rho[half_index-1:]) * radius
+
+    dot_vertices = np.dot(vertices, direction)
+    p1_mask = dot_vertices <= 0
+    p2_mask = dot_vertices > 0
+    vertices[p1_mask] = vertices[p1_mask] + np.array(p1)
+    vertices[p2_mask] = vertices[p2_mask] + np.array(p2)
+
+    vertices_path = np.ones((vertices_num + 3, 2))
+    vertices_path[0, :] = vertical_point_1
+    vertices_path[-1, :] = vertical_point_1
+    vertices_path[half_index + 1, :] = vertical_point_2
+
+    vertices_path[1:half_index + 1, :] = vertices[:half_index, :]
+    vertices_path[half_index + 2:-1, :] = vertices[half_index:, :]
+    return vertices_path
+
+
+def cell_patches(radius, p1, p2, face_color: str, num=40):
+    """
+
+    Parameters
+    ----------
+    radius: float
+        cell radius
+    p1: tuple or list
+        first sphere (x, y)
+    p2: tuple or list
+        second sphere (x, y)
+    facecolor: string or color code
+        cell color
+    num
+
+    Returns
+    -------
+
+    """
+    return mpatches.PathPatch(mpath.Path(cell_vertices(radius, p1, p2)), facecolor=face_color, edgecolor='k', alpha=.6)
+
+
+# %%
+cells_path = r'.\CellMD3D_visulization\example_cell_data.txt'
+cells = read_cells_rets(cells_path)
+
+cells_patches = [cell_patches(0.7, cell.p[:-1], cell.q[:-1], 'g') for cell in cells]
+
+# radius = .1
+#
+# p1 = (5.1, 2.1)
+# p2 = (5.2, 2.2)
+#
+# vertices_path = cell_vertices(radius, p1, p2)
+
+# cell_path = mpath.Path(vertices_path)
+# cell_patch = mpatches.PathPatch(cell_path, facecolor='g', edgecolor='k')
+fig1, ax1 = plt.subplots(1)
+# ax1.scatter(vertices[:, 0], vertices[:, 1])
+for cell_p in cells_patches:
+
+    ax1.add_patch(cell_p)
+# ax1.set_xlim(0, 10)
+# ax1.set_ylim(0, 10)
+ax1.set_xlim(-150, 150)
+ax1.set_ylim(-150, 150)
+
+fig1.show()
---
+++ CellMD3D_visulization/CellMD3D_utility.py
@@ -0,0 +1,141 @@
+from matplotlib.colors import ListedColormap, LinearSegmentedColormap
+import sciplot as splt
+import numpy as np
+from tqdm import tqdm
+splt.whitegrid()
+import os
+import threading
+red = np.array([231, 76, 60]) / 255
+white = np.array([1, 1, 1])
+green = np.array([93, 173, 226]) / 255
+nodes = [0.0, .5, 1.0]
+
+RedGreen_cmap = LinearSegmentedColormap.from_list('RedGreen', list(zip(nodes, [red, white, green])))
+
+splt.whitegrid()
+# […]
+
+# Own modules
+
+paras_location = [0, 1, 2, slice(3, 6), slice(6, 9), 9, slice(10, 13), slice(13, 16), 16, slice(17, 20),
+                  slice(20, 23),
+                  23, 24, 25, 26, 27, 28, 29]
+
+
+# |t 0|ID|Type|p|q|Length|Tensor|Velocity|GrowthRate|DynFric|StaFric|time_p|time_q|age_p|age_q|Ancestor|G|R|
+
+def read_cells_rets(CellFilePath, pbar=True):
+    lines = []
+    if pbar:
+        p_bar = tqdm()
+    with open(CellFilePath) as ret_file:
+        while True:
+            line = ret_file.readline()
+            if line == '':
+                break
+            cell_pars = [float(par) for par in line.replace('\n', '').split(' ')]
+
+            cell_parameters = [np.array(cell_pars[index]) if isinstance(index, slice) else cell_pars[index]
+                               for index in paras_location]
+            lines.append(Cell(*cell_parameters))
+            if pbar:
+                p_bar.update()
+
+    return lines
+
+
+class Cell:
+
+    def __init__(self, t, ID, Type,  # 0, 1, 2
+                 p: np.array,  # 3:6
+                 q: np.array,  # 6:9
+                 Length,  # 9
+                 T: np.array,  # 10:13
+                 Velocity: np.array,  # 13:16
+                 GrowthRate,  # 16
+                 DynFric, StaFric,  # slice(17, 20), slice(20, 23),
+                 time_p, time_q,
+                 age_p, age_q,
+                 Ancestor, G, R):
+        self.t = t
+        self.ID = ID
+        self.Type = Type
+        self.p = p
+        self.q = q
+        self.Length = Length
+        self.T = T
+        self.Velocity = Velocity
+        self.GrowthRate = GrowthRate
+        self.DynFric = DynFric
+        self.StaFric = StaFric
+        self.time_p = time_p
+        self.time_q = time_q
+        self.age_p = age_p
+        self.age_q = age_q
+        self.Ancestor = Ancestor
+        self.G = G
+        self.R = R
+        self.state = None
+
+        self.center = self.p + self.q / 2
+
+
+def read_cells_rets(CellFilePath, pbar=True):
+    lines = []
+    if pbar:
+        p_bar = tqdm()
+    with open(CellFilePath) as ret_file:
+        while True:
+            line = ret_file.readline()
+            if line == '':
+                break
+            cell_pars = [float(par) for par in line.replace('\n', '').split(' ')]
+
+            cell_parameters = [np.array(cell_pars[index]) if isinstance(index, slice) else cell_pars[index]
+                               for index in paras_location]
+            lines.append(Cell(*cell_parameters))
+            if pbar:
+                p_bar.update()
+
+    return lines
+
+
+
+def loadAllCells(CellFilesPath):
+    """Load all cells data from directory.
+
+    Parameters:
+        CellFilesPath (str): the PATH for cells data.
+
+    Returns:
+        data (List[List[Cell]]): list(list(Cell obj)
+    """
+
+    def readCellTarget(CellFilePath, buff, index):
+        buff[index] = read_cells_rets(CellFilePath, False)
+
+        return None
+
+    filesDir = [file.name for file in os.scandir(CellFilesPath) if file.is_file()]
+    file_number = len(filesDir)
+
+    data = [None] * file_number
+    read_threads = [threading.Thread(target=readCellTarget, args=(os.path.join(CellFilesPath, path), data, i)) for
+                    i, path in enumerate(filesDir)]
+    for read_thread in read_threads:
+        read_thread.start()
+
+    pbar = tqdm(total=len(filesDir))
+    start_count = 0
+    while True:
+        count = 0
+        for i in data:
+            if i is not None:
+                count += 1
+        if start_count < count:
+            pbar.update(count - start_count)
+            start_count = count
+        if start_count == file_number:
+            break
+
+    return data
